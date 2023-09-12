2565499428427d658c03eedfb55bbdbededd46d3
Dante Derette <43002338+DanteDerette@users.noreply.github.com>
2023-09-12 20:24:02 -0300

    Update somente_numero_e_virgula.js

--- somente_numero_e_virgula.js
+++ somente_numero_e_virgula.js
@@ -1,11 +1,12 @@
-$('#').on('input', function () {
+// funcão em jQuery para deixar somente numeros e virgulas em um input 
+$('.myClass').on('input', function () {
     $(this).val(function (_, currentValue) {
-      // Remove all commas except the first one
+      
       var newValue = currentValue.replace(/,/g, function (match, offset, input) {
         return offset === input.lastIndexOf(',') ? match : '';
       });
       
-      // Remove any characters other than numbers and commas
+      
       return newValue.replace(/[^0-9,]/g, '');
     });
   });
