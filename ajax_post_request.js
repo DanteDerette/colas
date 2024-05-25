67611aded41bcccc87358b5fba09a772c999a480
Dante Derette <43002338+DanteDerette@users.noreply.github.com>
2024-05-25 16:17:23 -0300

    Update ajax_post_request.js

--- ajax_post_request.js
+++ ajax_post_request.js
@@ -9,3 +9,21 @@ $.ajax({
     
 });
 
+/////////////
+///// Ideia 2
+
+function frontend_to_backend(){
+    return $.ajax({
+         url: '/',
+         type: 'POST',
+         contentType: 'application/json',
+     }).done(function (retorno){
+         return retorno.dados
+     })
+}
+
+// Usabilidade
+frontend_to_backend().then(function (dados) {
+    console.log(dados);
+})
+
